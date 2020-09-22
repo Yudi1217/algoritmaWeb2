e386e1ff5f1630bac1e8da0838176568f3cbbfe5
Yudi1217 <67574175+Yudi1217@users.noreply.github.com>
2020-09-22 20:49:25 +0700

    Create variabel_biasa.php

---
+++ variabel_biasa.php
@@ -0,0 +1,17 @@
+<?php  
+$nama = "Achmad Wahyudi";
+$nim = 2018804472;
+$jurusan = "Sistem Informasi";
+$kampus = "STMIK INSAN PEMBANGUNAN";
+
+echo "Biodata Diri";
+echo "<br>";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "NIM Mahasiswa : $nim";
+echo "<br>";
+echo "Jurusan : $jurusan";
+echo "<br>";
+echo "Nama Kampus : $kampus";
+
+?>
